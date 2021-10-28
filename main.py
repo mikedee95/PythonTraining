53cd5956056ddd8628b9bdca1873479afe1286ea
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2021-10-28 15:54:49 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+__author__ = 'Mike'
+
+num1 = input('Enter number 1')
+num2 = input('Enter number 2')
+
+sum = float(num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Mike')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
